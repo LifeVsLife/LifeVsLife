6d99714cbebb4f2fb1a9586af626bcf01b3f834f
Moritz H. <hoeckermoritz@web.de>
2017-05-12 14:15:17 +0200

    müller

---
+++ BR.java
@@ -0,0 +1,55 @@
+
+
+//author: Moritz Höcker, Jonas Reichert
+//date: 08.05.2017
+//ver: 0.0.1
+
+import java.io.*;
+public class BR 
+{
+
+    String path;
+    public BR()
+    {
+        path = "I:/Schüler/Q11_Informatik/tester.txt";  //hier dateipfad ändern
+    }
+
+    public void writer(String text)
+    {
+        File file = new File(path);   
+        try {
+            FileWriter writer = new FileWriter(file, false); //false überschreibt true addiert
+            writer.write(text);
+            writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+
+            System.out.println(this.getClass().getName());
+        } 
+    }
+
+    public String reader()
+    {
+        try {
+
+            FileReader file = new FileReader(path);                        
+            BufferedReader br = new BufferedReader(file);
+
+            String line = null;
+            StringBuilder stringBuilder = new StringBuilder();
+            String ls = System.getProperty("line.separator");
+            
+            while((line = br.readLine()) != null)
+            {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            br.close();
+            //return line;
+            return stringBuilder.toString();
+        } catch (IOException ex) {
+            System.out.println(this.getClass().getName());
+            return null;
+        }
+     }
+ }
