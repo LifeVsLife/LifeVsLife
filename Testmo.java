16e1c93270ede9f11a04484c24251872be4ae07f
Moritz H. <hoeckermoritz@web.de>
2017-05-12 13:59:17 +0200

    meins

---
+++ Testmo.java
@@ -0,0 +1,28 @@
+
+
+public class Testmo
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Testmo
+     */
+    public Testmo()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
