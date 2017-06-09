612ef7051bed3011824eeaa696c24ee96cb52eeb
Tim Guggenmos <tim.guggenmos@me.com>
2017-06-10 00:14:56 +0200

    better JComponents

--- Cell.java
+++ Cell.java
@@ -1,12 +1,40 @@
 
+// author:   Marco Kist
+// date:     08.05.17
+// version:  0.0.3
+
 public class Cell
 {
-    int posX, posY;
+    private int playerId;
+    //private Lifetime lifetime;
+    //     private int cellColor;
 
-    public Cell(int posX, int posY) {
-        this.posX = posX;
-        this.posY = posY;
+    public Cell(int playerId)
+    {
+        this.playerId = playerId;
+        //playerColor = cellColor;
     }
+
+    public void setPlayerId()
+    {
+        this.playerId = playerId;
+    }
+
+    public int getPlayerId()
+    {
+        return playerId;
+    }
+
+    // solange der PlayerID-Wert größer als -1 ist ist die Zelle am Leben
+    public boolean isAlive()
+    {
+        return idIsAlive(playerId);
+    }
+
+    private static boolean idIsAlive(int id) {
+        return id > -1;
+    }
+
 
 
 }
---
+++ TPanel.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class TPanel extends JPanel {
+
+    public TPanel()
+    {
+
+    }
+
+}
