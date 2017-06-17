dfe7bcd7ab38c645b123a183bb6374017997b47c
Tim Guggenmos <tim.guggenmos@me.com>
2017-06-17 23:59:10 +0200

    listener implementation (basic)

---
+++ ui/listeners/key/TKeyListener.java
@@ -0,0 +1,24 @@
+
+package ui.listeners.key;
+
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+public class TKeyListener implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+}
---
+++ ui/listeners/mouse/TMouseListener.java
@@ -0,0 +1,34 @@
+
+package ui.listeners.mouse;
+
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+
+public class TMouseListener implements MouseListener {
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+}
