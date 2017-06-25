ca4fb6382f53b6675928e6daebbd6e4a6be47ac3
Tim Guggenmos <tim.guggenmos@me.com>
2017-06-25 12:42:25 +0200

    test

--- src/communication/clientTest.java
+++ src/communication/clientTest.java
@@ -7,6 +7,8 @@ import java.net.*;
 public class clientTest {
 
     Socket socket;
+    ObjectInputStream in;
+    ObjectOutputStream out;
 
     String ip = "";
     int port = 0;
@@ -17,9 +19,44 @@ public class clientTest {
         this.port = port;
     }
 
-    public void open() {
+    public clientTest()
+    {
+
+    }
+
+    public void send(String s)
+    {
         try {
+            System.out.println("Trying to send: '"+s+"'...");
+            out.writeObject(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void read()
+    {
+        try {
+            System.out.println("Trying to read...");
+            String s = (String) in.readObject();
+            System.out.println("Read: '"+s+"'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void open()
+    {
+        try {
+            System.out.println("Creating socket...");
             socket = new Socket(ip, port);
+            System.out.println("Creating in...");
+            in = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Creating out...");
+            out = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("Done: open()");
         } catch (IOException e) {
             e.printStackTrace();
         }
